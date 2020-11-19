133247ceecf496f5070fae266885950bc5a823a5
Tom Hayden <thayden@gmail.com>
2020-11-19 13:42:11 -0600

    adding the core analytics code

--- README.md
+++ README.md
@@ -11,12 +11,29 @@ The hngr pixel can be easily added to any site. It provides limited information 
 ```
 
 
-Vanilla JS
+Javascript Component
 ==================
-To include our vanilla javascript library you can just load the minified JS file from our CDN.
+To include our vanilla javascript library you can just load the minified JS file from our CDN. Put the following script tag in your header
 
 ```
 <script src="https://cdn.hngr.co/hngr.latest.min.js" crossorigin="anonymous"></script>
 
 ```
+
+And then, somewhere in your code, preferably near the script set your token like this:
+```
+  <script>
+    window.hngr.setKey('KEY')
+  </script>
+```
+
+Anywhere in your application, if you want to invoke an analytics call to hngr. Just invoke the `window.hngr.send` function:
+```
+    window.hngr.send('WEB', 'ORDER_PLACED', {'email': 'asdf@asdf.com'})
+```
+
+This function accepts three parameters:
+- Product Name - The product name should be `WEB` if it is a website, 'ORDERING' if it's an ordering site (or both), 'MOBILE' for mobile products
+- Event Name - This is the action/event that is happening
+- Payload - Any additional data to append to the request
 
---
+++ vanilla_js/hngr_analytics.js
@@ -0,0 +1,84 @@
+/*
+  vanilla JS library for hngr analytics
+  ham information service
+*/
+
+function getUrlParam(p) {
+  const param_dict = getUrlParams()
+  if (param_dict && param_dict[p]) {
+    return param_dict[p]
+  }
+  return null
+}
+
+function getUrlParams(url) {
+  let params = {}
+  let url_path = url
+
+  if (!url_path) {
+    url_path = window.location.href
+  }
+  const parts = url_path.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+    params[key] = value;
+  })
+  return params;
+}
+
+async function setKey(key='UNKNOWN') {
+  window.hngr.key = key
+}
+
+async function sendHam(productName='UNKNOWN', 
+  eventName='EXTERNAL', payload={}) { 
+
+  const hamURI = 'https://ham.hngr.co/ham.json'
+  const headers = {'Content-Type': 'application/json'}
+  let params = {'product': productName,
+    'event_name': eventName}
+  if (payload !== null) {
+    params['payload'] = btoa(JSON.stringify(payload))
+  }
+  var url = new URL(hamURI)
+
+  // support for the 5 major UTM parameters
+  const utm_ps = getUrlParam('utm_source')
+  if (utm_ps) {
+    params['utm_source'] = utm_ps
+  }
+  const utm_pm = getUrlParam('utm_medium')
+  if (utm_pm) {
+    params['utm_medium'] = utm_pm
+  }
+  const utm_pc = getUrlParam('utm_campaign')
+  if (utm_pc) { 
+    params['utm_campaign'] = utm_pc
+  }
+  const utm_ct = getUrlParam('utm_content')
+  if (utm_ct) {
+    params['utm_content'] = utm_ct
+  }
+  const utm_tt = getUrlParam('utm_term')
+  if (utm_tt) {
+    params['utm_term'] = utm_tt
+  }
+
+  // custom origin flag
+  params['origin'] = window.location.href
+  params['key'] = window.hngr.key
+
+  // note we will prob have to add a polyfill for this
+  url.search = new URLSearchParams(params).toString()
+  console.log(url.href)
+  fetch(url, {method: 'GET',
+    credentials: 'include',
+    headers: headers})
+    .then(res => {
+      res.json().then(json => {
+        const pid = json.ham_id
+      })
+    })
+    .catch(err => {console.log('ham err' + err)})
+}
+
+window.hngr = {}
+window.hngr.send = sendHam
